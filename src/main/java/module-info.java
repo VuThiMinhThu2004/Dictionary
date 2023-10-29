bb276d042c7627b2f6827308f9ba2275ae166a5e
minhthu04 <thu1234567890vu@gmail.com>
2023-10-29 14:25:36 +0700

    add models

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.dictionary {
     requires javafx.controls;
     requires javafx.fxml;
+    requires ormlite.jdbc;
+    requires java.sql;
 
 
     opens com.example.dictionary to javafx.fxml;
     exports com.example.dictionary;
+
+    exports Dictionary.Controllers to javafx.fxml;
 }
