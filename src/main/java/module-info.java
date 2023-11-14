c737d23779e07fa8f4a9f2103a39c7f045a260cd
minhthu04 <thu1234567890vu@gmail.com>
2023-11-14 21:36:27 +0700

    delete Login

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,7 +15,6 @@ module com.example.dictionary {
     requires httpclient;
     requires httpcore;
     requires json;
-    requires org.controlsfx.controls;
 
     opens com.example.dictionary to javafx.fxml;
     exports com.example.dictionary;
